5cef480bf6bb15a0d48cdb64113e2d46f136dbd5
anasolcentur <ana.sol.centur@gmail.com>
2025-07-10 22:51:21 -0300

    Registro de UsuarioPermitido y mejoras en admin de Solicitud

--- web/admin.py
+++ web/admin.py
@@ -1,21 +1,27 @@
 from django.contrib import admin
-from .models import Solicitud
+from .models import Solicitud, UsuarioPermitido
 from django.utils.html import format_html
 from django.db.models import Count
 
+# Registro del modelo UsuarioPermitido
+@admin.register(UsuarioPermitido)
+class UsuarioPermitidoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'codigo_validacion')
+    search_fields = ('nombre', 'email')
+
+# Registro del modelo Solicitud con estadísticas y resumen
 @admin.register(Solicitud)
 class SolicitudAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'email', 'categoria', 'mensaje_corto')
     list_filter = ('categoria',)
     search_fields = ('nombre', 'email', 'mensaje')
     ordering = ('-id',)
-    
+
     def mensaje_corto(self, obj):
         return (obj.mensaje[:50] + '...') if len(obj.mensaje) > 50 else obj.mensaje
     mensaje_corto.short_description = 'Mensaje'
 
     def changelist_view(self, request, extra_context=None):
-        # Cálculo de estadísticas
         total = Solicitud.objects.count()
         por_categoria = Solicitud.objects.values('categoria').annotate(cantidad=Count('id'))
 
@@ -28,7 +34,3 @@ class SolicitudAdmin(admin.ModelAdmin):
         extra_context['resumen_estadistico'] = format_html(resumen)
 
         return super().changelist_view(request, extra_context=extra_context)
-
-    def changelist_view_with_resumen(self, request, extra_context=None):
-        response = super().changelist_view(request, extra_context)
-        return response
